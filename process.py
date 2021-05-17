4947521348ffc3713a230d14335bf70b8ad6ae66
Jeff Kaufman <jefftk@google.com>
2021-05-17 09:18:49 -0400

    switch to namedtuple

--- process.py
+++ process.py
@@ -2,6 +2,10 @@ import csv
 import os
 import pytz
 from datetime import datetime
+import collections
+
+Record = collections.namedtuple(
+  "Record", ["year", "month", "day", "hr", "mn", "watts"])
 
 raw_data = []
 with open("tmp.dates") as dinf:
@@ -15,15 +19,17 @@ with open("tmp.dates") as dinf:
           if i:
             time, watts = line
             hr, mn = time.split(':')
-            raw_data.append(
-              (int(year), int(month), int(day), int(hr), int(mn), int(watts)))
+            raw_data.append(Record(
+              int(year), int(month), int(day), int(hr), int(mn), int(watts)))
 
 tz = pytz.timezone('America/New_York')
 dst_adj_data = []
-for year, month, day, hr, mn, watts in raw_data:
-  if tz.localize(datetime(year, month, day), is_dst=None).tzinfo._dst.seconds:
-    hr -= 1
-  dst_adj_data.append((year, month, day, hr, mn, watts))
+for record in raw_data:
+  if tz.localize(datetime(record.year, record.month, record.day),
+                 is_dst=None).tzinfo._dst.seconds:
+    record = Record(record.year, record.month, record.day,
+                    record.hr - 1, record.mn, record.watts)
+  dst_adj_data.append(record)
 
 #  2019-03-24 12:00 to 2019-03-27 23:00
 #  2019-04-10 13:00 to 2019-04-15 00:00
@@ -38,32 +44,28 @@ for year, month, day, hr, mn, watts in raw_data:
 #  2020-08-03 13:00 to 2020-08-05 15:00
 #  2020-08-08 16:00 to 2020-08-11 15:00
 broken_periods = [
-  ((2019,03,24,12), (2019,03,27,23)),
-  ((2019,04,10,13), (2019,04,15,00)),
-  ((2019,04,16,00), (2019,04,16,23)),
-  ((2019,04,24,00), (2019,04,24,23)),
-  ((2019,04,26,00), (2019,04,26,23)),
-  ((2019,08,17,15), (2019,08,28,11)),
-  ((2020,02,17,12), (2020,04,02,11)),
-  ((2020,05,10,13), (2020,05,16,17)),
-  ((2020,06,23,13), (2020,06,29,23)),
-  ((2020,07,02,16), (2020,07,12,13)),
-  ((2020,08,03,13), (2020,08,05,15)),
-  ((2020,08,08,16), (2020,08,11,15))]
+  ((2019,3,24,12), (2019,3,27,23)),
+  ((2019,4,10,13), (2019,4,15,0)),
+  ((2019,4,16,0),  (2019,4,16,23)),
+  ((2019,4,24,0),  (2019,4,24,23)),
+  ((2019,4,26,0),  (2019,4,26,23)),
+  ((2019,8,17,15), (2019,8,28,11)),
+  ((2020,2,17,12), (2020,4,2,11)),
+  ((2020,5,10,13), (2020,5,16,17)),
+  ((2020,6,23,13), (2020,6,29,23)),
+  ((2020,7,2,16),  (2020,7,12,13)),
+  ((2020,8,3,13),  (2020,8,5,15)),
+  ((2020,8,8,16),  (2020,8,11,15))]
 
-def is_broken(year, month, day, hr):
+def is_broken(record):
   for start, end in broken_periods:
-    if (start[0] <= year <= end[0] and
-        start[1] <= month <= end[1] and
-        start[2] <= day <= end[2] and
-        start[3] <= hr <= end[3]):
+    if (start[0] <= record.year <= end[0] and
+        start[1] <= record.month <= end[1] and
+        start[2] <= record.day <= end[2] and
+        start[3] <= record.hr <= end[3]):
       return True
   return False
 
-clean_data = []
-for year, month, day, hr, mn, watts in raw_data:
-  if not is_broken(year, month, day, hr):
-    clean_data.append((year, month, day, hr, mn, watts))
+clean_data = [record for record in raw_data
+              if not is_broken(record)]
 
-
-  
